45b59f6ee272a82fe97ef91ddba605a3813033a7
LurryLarry <lari.vesterinen@gmail.com>
2021-04-19 00:34:01 +0300

    tehtävä 3.12

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+console.log(process.argv.length)
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://lariv:${password}@cluster0.m9j6q.mongodb.net/puhelinluettelo?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: name,
+  number: number
+})
+
+if (process.argv.length === 3) {
+  console.log('phonebook:');
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+    process.exit(1)
+  })
+}
+
+person.save().then(result => {
+  console.log(`added ${result.name} number ${result.number} to phonebook`)
+  mongoose.connection.close()
+})
+
